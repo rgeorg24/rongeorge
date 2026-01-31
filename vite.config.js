81c16f2abf11d1eb9d5210961c7ee6db29d6e44b
rgeorg24 <rgeorg24@github.com>
2026-01-31 03:22:22 -0700

    Fix base path from portfolio-temp to rongeorge

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/portfolio-temp/',
+    base: '/rongeorge/',
     server: {
         port: 5173
     }
